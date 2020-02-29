729a42cc6c4785cd27a6c6afbad21edc77890808
qibang108 <53903949+qibang108@users.noreply.github.com>
2020-02-29 15:38:39 +0800

    Update charCount.c

--- charCount.c
+++ charCount.c
@@ -3,37 +3,36 @@
 
 /*统计输入字符个数及各字符出现次数(本程序仅用作统计小写字母,大写字母及数字算法类似)*/
 /*输入字符串以0结尾*/
-/*改进版使用getchar()进行单字符读写,回车后按Ctrl+Z结束读入*/
+/*改进版使用getchar()进行单字符读写,回车后按Ctrl+Z(Windows)结束读入*/
 
-int main() {
+int main(int argc, const char *argv[]) {
 
 	char c;
 	int count[26] = {0};    //用于存放各小写字符出现次数
 	int num = 0;            //字符串长度
 
-/*	
-	scanf_s("%c", &c);
-
+/*旧版本需要输入字符串以0结尾	
+	scanf("%c", &c);
 	while (c != '0')
 	{
 		count[c - 'a']++;
-		scanf_s("%c", &c);
+		scanf("%c", &c);
 		num++;
 	}
 */
 
-    while ((c=getchar()) != EOF)
+    	while ((c=getchar()) != EOF)
 	{
 		count[c - 'a']++;
 		num++;
 	}
 
-	printf_s("共输入%d个字符，其中：\n", num-1);
+	printf("共输入%d个字符，其中：\n", num-1);
 
 	for (int i = 0; i < 26; i++)
 	{
 		if (count[i] != 0)
-			printf_s("'%c' 出现 %d 次\n", i + 97, count[i]);
+			printf("'%c' 出现 %d 次\n", i + 97, count[i]);
 	}
 	
 	system("pause");
